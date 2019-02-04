bd453abe7a732f9480041c25c0f139c8c2b7fb38
tawfikm <tawfikmourchid@gmail.com>
2019-02-04 02:05:39 +0100

    before swapping

---
+++ src/Swap.java
@@ -0,0 +1,23 @@
+/**
+ * Created by MOURCHID Tawfik  on 2/4/2019,  at 02h04
+ */
+public class Swap {
+    public static  void  main(String args[]){
+
+        int x = 43;
+        int y = 20;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+
+        System.out.println("swapping...");
+
+        // swapping
+
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+
+    }
+}
